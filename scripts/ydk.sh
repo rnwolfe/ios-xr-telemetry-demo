23f79bf313450b8ad2d7e40dd60f8df8cd9e75b1
Ryan Wolfe <rn.wolfe@gmail.com>
2018-08-20 15:17:05 -0400

    clean up ydk provision

--- scripts/ydk.sh
+++ scripts/ydk.sh
@@ -1,18 +1,26 @@
 # Essentials
-sudo apt-get -y update
-sudo apt-get -y install default-jdk
+echo "Installing Java.."
+sudo apt-get -y -q install default-jdk
+echo "Updating packages.."
+sudo apt-get -y -q update
 
-sudo pip install kafka-python
+echo "Updating pip.."
+sudo pip install -U -q pip
+echo "Installing kafka-python module.."
+sudo pip install -q kafka-python
 
 # Add route supernet for networks in use by IOS-XR topology without breaking internet default route.
+echo "Add route to virtual router networks.."
 sudo route add -net 10.0.0.0/8 gw 10.1.1.1
 
 # Get, configure, and run pipeline
+echo "Get, configure, and run pipeline.."
 mkdir /vagrant/pipeline
 git clone -q https://github.com/cisco/bigmuddy-network-telemetry-pipeline.git /vagrant/pipeline
 /vagrant/pipeline/bin/pipeline -config /vagrant/init-configs/pipeline.conf -log /vagrant/pipeline/pipeline.log 2> /dev/null &
 
 # Get, configure, and run kafka
+echo "Get, configure, and run kafka/zookeeper.."
 wget -q http://apache.claz.org/kafka/2.0.0/kafka_2.11-2.0.0.tgz
 mkdir /vagrant/kafka
 tar -xzf kafka_2.11-2.0.0.tgz -C /vagrant/kafka
