e0b571e3782f552a367d764698c7f93f308e6db8
Ryan Wolfe <rn.wolfe@gmail.com>
2018-08-23 14:13:59 -0400

    specify YDK VM

--- scripts/ydk.sh
+++ scripts/ydk.sh
@@ -17,7 +17,7 @@ sudo docker pull akshshar/pipeline-kafka
 # Run container instance with needed ports exposed and pushing our pipeline.conf file into it.
 docker run -itd --name pk -v /vagrant/scripts/:/root/ -v /vagrant/init-configs/pipeline.conf:/data/pipeline.conf -p 5432:5432 -p 5958:5958 -p 9092:9092 -p 2181:2181 akshshar/pipeline-kafka
 
-# Once on the VM, get on the container and run scripts using:
+# Once on the YDK VM, get on the container and run scripts using:
 #   docker exec -it pk bash
 #   cd /root
 #   python show-bytes-on-change.py
